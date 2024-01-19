19a872d9f62c892e6ffe8bc6bb006e08e82ad4a7
EightonLeePaulse12 <eightonleepaulse@gmail.com>
2024-01-19 15:05:24 +0200

    yes

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,32 @@
+'use client';
+
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Swal from 'sweetalert2'
 const page = () => {
 
+  const [todos, setToDo] = useState();
   useEffect(()=>{
-    const fetchData = async () =>{
-      const res = await axios.get("/api/getToDos")
-      console.log(res.data)
-      
+
+    try {
+      const fetchData = async () =>{
+        const res = await axios.get("/api/getToDos")
+        const todo = res.data
+        console.log(res.data)
+        setToDo(todo)
+      }
+
+      fetchData()
+    } catch (error) {
+      console.log(error)
     }
+    
+    
   }, [])
 
   return (
     <>
-    <p>{  }</p>
+    <p>{ todos }</p>
     <div className="text-red-400">page</div>
     </>
   )
