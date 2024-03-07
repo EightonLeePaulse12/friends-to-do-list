94ddb5d3598eee4aeecefc7ed4eb60b44219ae0b
EightonLeePaulse12 <eightonleepaulse@gmail.com>
2024-03-07 13:22:56 +0200

    busy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,115 @@
-import React from 'react'
+'use client';
+
+import React, { useEffect, useState } from 'react'
 // import { Progress } from '~/components/ui/progress';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "~/components/ui/table"
+import { useSession } from 'next-auth/react'
+import { todoRouter } from '~/server/api/routers/todo'
+
 
 const page = () => {
+  useSession({ required: true })
+  // const session = useSession();
+  console.log()
+  const [todos, setToDos] = useState<string[]>([])
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      // try{
+      //   const { data, error } = await todoRouter.fetchToDos({
+          
+      //   })
+      //   if(error) {
+      //     console.log("Error: ", error)
+      //   } else{
+      //     setToDos(data)
+      //     console.log(todos)
+      //   }
+      // } catch(e) {
+
+      // }
+    }
+  }, [])
   return (
     <>
-      
-      <div>Hello, World!</div>
+
+      {/* model Todos {
+    id       Int        @id @default(autoincrement())
+    userId   String
+    user     User       @relation(fields: [userId], references: [id], onDelete: Cascade, onUpdate: Cascade)
+    name     String
+    priority Priority
+    subnotes Subnotes[]
+    dueDate  DateTime?
+    createdAt DateTime @default(now())
+
+    @@index([userId])
+} */}
+      <div className="container">
+        <div className="header">
+          <h1 className='text-center p-8 font-bold text-[48px]'>To-do List</h1>
+        </div>
+        <div className="">
+          <Table>
+            <TableCaption>Manage Your Day!</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead>ID</TableHead>
+                <TableHead>Name</TableHead>
+                <TableHead>Due Date</TableHead>
+                <TableHead>Created At</TableHead>
+                <TableHead>Sub Notes</TableHead>
+                <TableHead>Due Date</TableHead>
+                <TableHead>Priority</TableHead>
+                <TableHead>Edit</TableHead>
+                <TableHead>Delete</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  Yo
+                </TableCell>
+                <TableCell>
+                  Yo
+                </TableCell>
+                <TableCell>
+                  Yo
+                </TableCell>
+                <TableCell>
+                  Yo
+                </TableCell>
+                <TableCell>
+                  Yo
+                </TableCell>
+                <TableCell>
+                  Yo
+                </TableCell>
+                <TableCell>
+                  Yo
+                </TableCell>
+                <TableCell>
+                  Yo
+                </TableCell>
+                <TableCell>
+                  Yo
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </div>
+      </div>
     </>
   )
 }
 
 export default page
+
--- src/server/api/routers/todo.ts
+++ src/server/api/routers/todo.ts
@@ -14,31 +14,158 @@ export const todoRouter = createTRPCRouter({
       where: {
         userId: userID,
       },
-    });
-    return todos
-  }),
-  fetchSingleToDo: protectedProcedure.input(
-    z.object({
-      id: z.number()
-    })
-  ).query(async ({ ctx, input }) => {
-    const todo = await ctx.db.todos.findUnique({
-      where: {
-        id: input.id
-      },
       include: {
-        subnotes: {
-          where: {
-            todoId: input.id
-          }
-        }
-      }
-    })
+        subnotes: true,
+      },
+    });
+    return todos;
   }),
-  makeToDo: protectedProcedure.input(
-    z.object({
-      id: z.number(),
+  fetchSingleToDo: protectedProcedure
+    .input(
+      z.object({
+        id: z.number(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const todo = await ctx.db.todos.findUnique({
+        where: {
+          id: input.id,
+        },
+        include: {
+          subnotes: {
+            where: {
+              todoId: input.id,
+            },
+          },
+        },
+      });
+    }),
+  makeToDo: protectedProcedure
+    .input(
+      z.object({
+        id: z.number(),
+        userId: z.string(),
+        name: z.string(),
+        priority: z.enum(["Null", "Low", "Neutral", "High", "Critical"]),
+        dueDate: z.date().nullish(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const createToDo = await ctx.db.todos.create({
+        data: {
+          userId: input.userId,
+          name: input.name,
+          priority: input.priority,
+          dueDate: input.dueDate,
+        },
+      });
+      return createToDo;
+    }),
+  createSubNotes: protectedProcedure
+    .input(
+      z.object({
+        subnotes: z.array(z.object({ name: z.string() })),
+        todoId: z.number(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { todoId, subnotes } = input;
 
-    })
-  )
+      if (!todoId || subnotes.length === 0 || subnotes.length < 0) {
+        throw new Error(
+          "Something went wrong, either there were no subnotes or an ID wasn't provided.",
+        );
+      }
+
+      const createSubNote = await ctx.db.subnotes.createMany({
+        data: subnotes.map((sub) => ({
+          name: JSON.stringify(sub.name),
+          todoId,
+        })),
+      });
+
+      return createSubNote;
+    }),
+  editToDo: protectedProcedure
+    .input(
+      z.object({
+        id: z.number(),
+        name: z.string(),
+        priority: z.enum(["Null", "Low", "Neutral", "High", "Critical"]),
+        dueDate: z.date().nullish(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const edit = await ctx.db.todos.update({
+        data: {
+          name: input.name,
+          priority: input.priority,
+          dueDate: input.dueDate,
+        },
+        where: {
+          id: input.id,
+        },
+      });
+
+      return edit;
+    }),
+  editSubnotes: protectedProcedure
+    .input(
+      z.object({
+        subnotes: z.string(),
+        todoId: z.number(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { todoId, subnotes } = input;
+      if (!todoId || subnotes.length === 0 || subnotes.length < 0) {
+        throw new Error(
+          "Something went wrong, either there were no subnotes or an ID wasn't provided.",
+        );
+      }
+      const edit = ctx.db.subnotes.update({
+        where: {
+          id: todoId,
+        },
+        data: {
+          name: subnotes,
+        },
+      });
+
+      return edit;
+    }),
+  deleteToDo: protectedProcedure
+    .input(
+      z.object({
+        id: z.number(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { id } = input;
+      if (!id) throw new Error("Something went wrong, no todo ID provided");
+
+      const deletion = await ctx.db.todos.delete({
+        where: {
+          id: id,
+        },
+      });
+      return deletion;
+    }),
+  deleteSubNote: protectedProcedure
+    .input(
+      z.object({
+        id: z.number(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { id } = input;
+      if (!id) throw new Error("Something went wrong, no ID was provided");
+
+      const deletion = await ctx.db.subnotes.delete({
+        where: {
+          id: id,
+        },
+      });
+      return deletion;
+    }),
 });
